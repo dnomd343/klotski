c67730aa25a1479d17dec6c65c7bc6c8e432c3ea
Dnomd343 <i@343.re>
2023-01-05 23:58:10 +0800

    perf: AllCases module

--- klotski/all_cases.cc
+++ klotski/all_cases.cc
@@ -2,6 +2,31 @@
 #include "common.h"
 #include "all_cases.h"
 
+AllCases::AllCases(enum InitType init) {
+    switch (init) {
+        case InitType::WITH_ALL_CASES:
+            get_all_cases();
+        case InitType::WITH_BASIC_RANGES:
+            get_basic_ranges();
+        case WITH_NOTHING:
+            break;
+    }
+}
+
+const std::vector<uint32_t>* AllCases::get_basic_ranges() {
+    if (basic_ranges.empty()) {
+        build_basic_ranges(); // basic ranges initialize
+    }
+    return &basic_ranges;
+}
+
+const std::vector<uint32_t> (*AllCases::get_all_cases())[16] {
+    if (all_cases->empty()) {
+        find_all_cases(); // all cases initialize
+    }
+    return &all_cases;
+}
+
 inline uint32_t binary_count(uint32_t bin) { // get number of non-zero bits
     bin -= (bin >> 1) & 0x55555555;
     bin = (bin & 0x33333333) + ((bin >> 2) & 0x33333333);
@@ -10,6 +35,37 @@ inline uint32_t binary_count(uint32_t bin) { // get number of non-zero bits
     bin += bin >> 8;
     bin += bin >> 16;
     return bin & 0b111111;
+}
+
+void AllCases::find_all_cases() { // find all valid cases
+    get_basic_ranges(); // ensure basic ranges exist
+    for (uint32_t head = 0; head < 16; ++head) { // address of 2x2 block
+        if ((head & 0b11) == 0b11) {
+            continue; // invalid 2x2 address
+        }
+        for (uint32_t range : basic_ranges) { // check base on 2x2 address and range
+            if (Common::check_case(head, range)) {
+                all_cases[head].emplace_back(Common::range_reverse(range)); // found valid case
+            }
+        }
+    }
+}
+
+void AllCases::build_basic_ranges() { // build basic ranges
+    for (int n = 0; n <= 7; ++n) { // number of 1x2 and 2x1 block -> 0 ~ 7
+        for (int n_2x1 = 0; n_2x1 <= n; ++n_2x1) { // number of 2x1 block -> 0 ~ n
+            for (int n_1x1 = 0; n_1x1 <= (14 - n * 2); ++n_1x1) { // number of 1x1 block -> 0 ~ (14 - 2n)
+                int n_1x2 = n - n_2x1;
+                int n_space = 16 - n * 2 - n_1x1;
+                /// 0x0 -> 00 | 1x2 -> 01 | 2x1 -> 10 | 1x1 -> 11
+                generate_ranges(n_space, n_1x2, n_2x1, n_1x1); // generate target ranges
+            }
+        }
+    }
+    std::sort(basic_ranges.begin(), basic_ranges.end()); // sort basic ranges
+    for (uint32_t &range : basic_ranges) {
+        range = Common::range_reverse(range); // range reverse
+    }
 }
 
 void AllCases::generate_ranges(int n1, int n2, int n3, int n4) { // generate target ranges
@@ -74,60 +130,4 @@ void AllCases::generate_ranges(int n1, int n2, int n3, int n4) { // generate tar
             basic_ranges.emplace_back(range); // insert into release ranges
         }
     }
-}
-
-void AllCases::build_basic_ranges() { // build basic ranges
-    for (int n = 0; n <= 7; ++n) { // number of 1x2 and 2x1 block -> 0 ~ 7
-        for (int n_2x1 = 0; n_2x1 <= n; ++n_2x1) { // number of 2x1 block -> 0 ~ n
-            for (int n_1x1 = 0; n_1x1 <= (14 - n * 2); ++n_1x1) { // number of 1x1 block -> 0 ~ (14 - 2n)
-                int n_1x2 = n - n_2x1;
-                int n_space = 16 - n * 2 - n_1x1;
-                /// 0x0 -> 00 | 1x2 -> 01 | 2x1 -> 10 | 1x1 -> 11
-                generate_ranges(n_space, n_1x2, n_2x1, n_1x1); // generate target ranges
-            }
-        }
-    }
-    std::sort(basic_ranges.begin(), basic_ranges.end()); // sort basic ranges
-    for (uint32_t &range : basic_ranges) {
-        range = Common::range_reverse(range); // range reverse
-    }
-}
-
-void AllCases::find_all_cases() { // find all valid cases
-    get_basic_ranges(); // ensure basic ranges exist
-    for (uint32_t head = 0; head < 16; ++head) { // address of 2x2 block
-        if ((head & 0b11) == 0b11) {
-            continue; // invalid 2x2 address
-        }
-        for (uint32_t range : basic_ranges) { // check base on 2x2 address and range
-            if (Common::check_case(head, range)) {
-                all_cases[head].emplace_back(Common::range_reverse(range)); // found valid case
-            }
-        }
-    }
-}
-
-AllCases::AllCases(enum InitType init) {
-    switch (init) {
-        case InitType::WITH_ALL_CASES:
-            get_all_cases();
-        case InitType::WITH_BASIC_RANGES:
-            get_basic_ranges();
-        case WITH_NOTHING:
-            break;
-    }
-}
-
-const std::vector<uint32_t>* AllCases::get_basic_ranges() {
-    if (basic_ranges.empty()) {
-        build_basic_ranges(); // basic ranges initialize
-    }
-    return &basic_ranges;
-}
-
-const std::vector<uint32_t> (*AllCases::get_all_cases())[16] {
-    if (all_cases->empty()) {
-        find_all_cases(); // all cases initialize
-    }
-    return &all_cases;
 }
