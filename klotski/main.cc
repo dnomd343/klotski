40a616c97abc11db5e2db834b8536c7b5545ec19
Dnomd343 <i@343.re>
2023-01-07 00:21:17 +0800

    remove: short code verify script

--- klotski/main.cc
+++ klotski/main.cc
@@ -3,78 +3,33 @@
 #include "short_code.h"
 //#include "common_code.h"
 
-int main(int argc, char *argv[]) {
+int main() {
 
 //    auto a = AllCases();
-//    auto a = AllCases(AllCases::InitType::WITH_NOTHING);
-//    auto a = AllCases(AllCases::InitType::WITH_BASIC_RANGES);
-//    auto a = AllCases(AllCases::InitType::WITH_ALL_CASES);
-
-//    std::cout << "start get basic ranges" << std::endl;
+//    auto a = AllCases(AllCases::Build::BASIC_RANGES);
+//    auto a = AllCases(AllCases::Build::ALL_CASES);
+//
+//    std::cout << "start getting basic ranges" << std::endl;
 //    std::cout << "basic range: " << a.get_basic_ranges()->size() << std::endl;
-
-//    std::cout << "start get all cases" << std::endl;
+//
+//    std::cout << "start getting all cases" << std::endl;
 //    for (const auto &temp : *a.get_all_cases()) {
-//        std::cout << temp.size() << std::endl;
+//        std::cout << "  " << temp.size() << std::endl;
 //    }
 
 
-//    auto s = ShortCode();
+    auto s = ShortCode();
 //    auto s = ShortCode(ShortCode::Mode::NORMAL);
 //    auto s = ShortCode(ShortCode::Mode::FAST);
 
 //    s.speed_up(ShortCode::Mode::NORMAL);
 //    s.speed_up(ShortCode::Mode::FAST);
 
-//    printf("%d\n", s.zip_short_code(0x6EC0F8800));
-//    printf("%09lX\n", s.unzip_short_code(14323231));
+    printf("%d\n", s.zip_short_code(0x6EC0F8800));
+    printf("%09lX\n", s.unzip_short_code(14323231));
 
-
-    uint32_t start_code = atoi(argv[1]);
-    uint32_t end_code = atoi(argv[2]);
-
-//    std::cout << "preparing..." << std::endl;
-    std::vector<uint64_t> all_cases;
-    auto a = AllCases(AllCases::ALL_CASES);
-    for (int head = 0; head < 16; ++head) {
-        uint64_t prefix = (uint64_t)head << 32;
-        for (const auto &range : (*a.get_all_cases())[head]) {
-            all_cases.emplace_back(prefix | range);
-        }
-    }
-
-    auto s = ShortCode(ShortCode::Mode::NORMAL);
-//    auto s = ShortCode(ShortCode::Mode::FAST);
-//    std::cout << "start working" << std::endl;
-
-    for (auto short_code = start_code; short_code < end_code + 1; ++short_code) {
-
-        uint64_t common_code = all_cases[short_code];
-
-        if (short_code != s.zip_short_code(common_code)) {
-            printf("ERROR: zip %d\n", short_code);
-        }
-        if (common_code != s.unzip_short_code(short_code)) {
-            printf("ERROR: unzip %09lX\n", common_code);
-        }
-
-//        if (short_code % 2000000 == 0) {
-//        if (short_code % 10000 == 0) {
-//            std::cout << ((float)short_code / (float)all_cases.size() * 100) << "%" << std::endl;
-//        }
-    }
-//    std::cout << "complete verify" << std::endl;
-    std::cout << "[" << start_code << ", " << end_code << "]," << std::endl;
-
-
-//    for (uint32_t short_code = 0; short_code < 29334498; ++short_code) {
-//        if (short_code != ShortCode::code_from_string(ShortCode::code_to_string(short_code))) {
-//            std::cout << "ERROR: " << short_code << std::endl;
-//        }
-//    }
-
-//    std::cout << ShortCode::code_to_string(14323231) << std::endl;
-//    std::cout << ShortCode::code_from_string("EP4HZ") << std::endl;
+    std::cout << ShortCode::code_to_string(14323231) << std::endl;
+    std::cout << ShortCode::code_from_string("EP4HZ") << std::endl;
 
     return 0;
 }
