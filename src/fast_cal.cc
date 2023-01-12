745844cc9b21c3cedbc8dd9b2d418c51e0325175
Dnomd343 <i@343.re>
2023-01-12 10:21:34 +0800

    feat: test global break point

--- src/fast_cal.cc
+++ src/fast_cal.cc
@@ -17,7 +17,13 @@ std::queue<fast_cal_t*> cache;
 
 std::unordered_map<uint64_t, fast_cal_t> cases;
 
+//bool stop_flag;
+
 void add_new_case(uint64_t code, uint64_t mask) {
+
+//    if (stop_flag) {
+//        return;
+//    }
 
     auto exist_case = cases.find(code);
     if (exist_case != cases.end()) { // find existed case
@@ -30,11 +36,18 @@ void add_new_case(uint64_t code, uint64_t mask) {
     cases[code] = fast_cal_t {
         .code = code,
         .mask = mask,
+//        .step = cache.front()->step + 1,
         .last = cache.front(),
     };;
     cache.emplace(&cases[code]);
 
     // TODO: try to check head address = 0xD at here
+
+//    if (((code >> (3 * 0xD)) & 0b111) == B_2x2) {
+//        std::cout << "Resolved" << std::endl;
+//            std::cout << RawCode(cache.front()->code).dump_case() << std::endl;
+//        stop_flag = true;
+//    }
 
 }
 
@@ -46,20 +59,28 @@ uint32_t fast_cal(uint64_t code) {
 
     cache.empty();
 
+//    stop_flag = false;
+
     cases[code] = fast_cal_t {
         .code = code,
         .mask = 0,
+//        .step = 0,
         .last = nullptr,
     };
     cache.emplace(&cases[code]);
 
     while (!cache.empty()) {
 
+        // break check point
         if (((cache.front()->code >> (3 * 0xD)) & 0b111) == B_2x2) {
             std::cout << "Resolved" << std::endl;
 //            std::cout << RawCode(cache.front()->code).dump_case() << std::endl;
             break;
         }
+
+//        if (stop_flag) {
+//            break;
+//        }
 
         core.next_step(cache.front()->code, cache.front()->mask);
         cache.pop();
