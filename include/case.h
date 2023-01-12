825abf8d7b657b9877c12282dfc92f2eaa190739
Dnomd343 <i@343.re>
2023-01-12 15:24:25 +0800

    remove: some legacy code

--- include/case.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CASE_H_
-#define _CASE_H_
-
-#include <cstdint>
-#include <vector>
-
-void find_all_case(std::vector<uint64_t> *all_case);
-
-#endif
--- include/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef _COMMON_H_
-#define _COMMON_H_
-
-#include <cstdint>
-
-void binary_reverse(uint32_t &range);
-void binary_to_str(uint64_t binary, char *string);
-
-#endif
