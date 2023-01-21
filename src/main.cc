7a5662de3b2d69f7fc31c3c779eccc737974a878
Dnomd343 <i@343.re>
2023-01-22 01:57:31 +0800

    remove: legacy svg graph code

--- src/main.cc
+++ src/main.cc
@@ -18,7 +18,7 @@
 //int main(int argc, char *argv[]) {
 int main() {
 
-//    BasicRanges::build();
+    BasicRanges::build();
 //    AllCases::build();
 
 
@@ -59,73 +59,36 @@ int main() {
 //    }
 
 
-//    auto a = Analyse(RawCode::from_common_code("1a9bf0c"));
-//    auto a = Analyse(RawCode::from_common_code("4fea134"));
-//    auto a = Analyse(RawCode::from_common_code("A5D3AF"));
-//    auto a = Analyse(RawCode::from_common_code("1FA6F80"));
-//    auto a = Analyse(RawCode::from_common_code("0F5747"));
-
-//    if (argc < 2) {
-//        std::cout << "without input code" << std::endl;
-//    }
-//    auto a = Analyse(RawCode::from_common_code(argv[1]));
-
-
-    std::string input;
-    std::cout << "Please give a common code: ";
-    std::cin >> input;
-
-    auto common_code = CommonCode::from_string(input);
-
-    std::cout << ShortCode::from_common_code(common_code).to_string() << " -> ";
-
     auto start_time = clock();
 
-    auto a = Analyse(RawCode::from_common_code(common_code));
+//    auto a = Analyse(RawCode::from_common_code("1a9bf0c"));
+//    auto a = Analyse(RawCode::from_common_code("4fea134"));
+    auto a = Analyse(RawCode::from_common_code("A5D3AF"));
+
 
 //    a.build();
     auto ret = a.build_until([](uint64_t code) {
         return ((code >> (3 * 0xD)) & 0b111) == B_2x2;
     });
-    std::cout << ret.size() << std::endl;
-
-    std::cerr << (clock() - start_time) * 1000000 / CLOCKS_PER_SEC << "us" << std::endl;
-
-//    std::cout << "analyse complete" << std::endl;
 //    for (const auto &r : ret) {
 //        std::cout << r << std::endl;
 //    }
+//    std::cout << ret.size() << std::endl;
 
-//    auto ret = a.backtrack({
-//        0x7F87E0E5BFFF492, 0x1FB1E36F9FFF492, RawCode::from_common_code("2CEA8DC").unwrap()
-//    });
 
-    auto svg_ret = a.backtrack(ret);
+    std::cerr << (clock() - start_time) * 1000000 / CLOCKS_PER_SEC << "us" << std::endl;
 
-//    std::cout << "backtrack complete" << std::endl;
 
     auto g = Graph();
+    auto svg_ret = a.backtrack(ret);
     auto svg_data = g.svg_demo(svg_ret);
 
+    std::cout << svg_data << std::endl;
 
-    std::ofstream output;
-    output.open(common_code.to_string(true) + ".svg");
-    output << svg_data << std::endl;
-    output.close();
-
-//    std::cout << svg_data << std::endl;
-
-//    auto start_time = clock();
 
 //    std::cout << a.layer_export(81).size() << std::endl;
 //    std::cout << a.layer_export()[81].size() << std::endl;
 
-//    auto raw_code = CommonCode("A5D3AF0").to_raw_code().unwrap();
-
-//    auto a = Analyse();
-//    a.start_analyse(raw_code);
-
-//    a.backtrack(0x07F87E0E5BFFF492);
 
 //    std::vector<uint64_t> raw_codes = {
 //        CommonCode(0xDAAF4CC00).to_raw_code().unwrap(),
@@ -159,12 +122,11 @@ int main() {
 
 //    std::cerr << (clock() - start_time) / CLOCKS_PER_SEC << "s" << std::endl;
 //    std::cerr << (clock() - start_time) * 1000 / CLOCKS_PER_SEC << "ms" << std::endl;
-//    std::cerr << (clock() - start_time) * 1000000 / CLOCKS_PER_SEC << "us" << std::endl;
+    std::cerr << (clock() - start_time) * 1000000 / CLOCKS_PER_SEC << "us" << std::endl;
 
 //    std::cout << "complete benchmark" << std::endl;
 
-    std::cerr << "Press CTRL-C for exit...";
-    pause();
+//    pause();
 
     return 0;
 }
