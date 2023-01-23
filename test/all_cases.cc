855ab61e7347f6de6198b1011d4458456beb1de1
Dnomd343 <i@343.re>
2023-01-24 00:26:45 +0800

    test: enable md5 hash check

--- test/all_cases.cc
+++ test/all_cases.cc
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include "md5.h"
 #include "all_cases.h"
 #include "gtest/gtest.h"
 
@@ -7,4 +8,12 @@ const uint32_t BASIC_RANGES_SIZE = 7311921;
 TEST(AllCases, basic_ranges_size) {
     auto &basic_ranges = BasicRanges::fetch();
     EXPECT_EQ(basic_ranges.size(), BASIC_RANGES_SIZE);
+}
+
+TEST(AllCases, basic_ranges_data) {
+    auto &basic_ranges = BasicRanges::fetch();
+
+    std::cout << md5("dnomd343", 8) << std::endl;
+
+//    EXPECT_EQ(basic_ranges.size(), BASIC_RANGES_SIZE);
 }
