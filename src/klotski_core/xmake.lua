d583a09cb2ea2681a47888130bb46897d5dcc903
Dnomd343 <i@343.re>
2023-01-29 16:50:48 +0800

    build: xmake metadata template

--- src/klotski_core/xmake.lua
+++ src/klotski_core/xmake.lua
@@ -1,11 +1,12 @@
-set_xmakever("2.0.0")
+----------------------------------------------------------------
 
+set_xmakever("2.5.3")
+
+set_project("klotski-core")
 set_version("0.1.2")
-
 set_languages("cxx14")
 
 set_optimize("fastest")
-
 set_warnings("everything")
 
 ----------------------------------------------------------------
@@ -26,67 +27,73 @@ add_includedirs("benchmark")
 ----------------------------------------------------------------
 
 target("utils")
-    set_kind("static")
+    set_kind("object")
     add_files("utils/*.cc")
 
 target("all_cases")
-    set_kind("static")
+    set_kind("object")
     add_files("all_cases/*.cc")
 
 target("raw_code")
-    set_kind("static")
+    set_kind("object")
     add_files("raw_code/*.cc")
 
 target("short_code")
-    set_kind("static")
+    set_kind("object")
     add_includedirs("short_code/offset")
     add_files("short_code/*.cc")
 
 target("common_code")
-    set_kind("static")
+    set_kind("object")
     add_files("common_code/*.cc")
 
 target("core")
-    set_kind("static")
+    set_kind("object")
     add_files("core/*.cc")
 
 target("fast_cal")
-    set_kind("static")
+    set_kind("object")
     add_files("fast_cal/*.cc")
 
 target("analyse")
-    set_kind("static")
+    set_kind("object")
     add_files("analyse/*.cc")
 
 target("benchmark")
-    set_kind("static")
+    set_kind("object")
     add_files("benchmark/*.cc")
 
 ----------------------------------------------------------------
 
--- TODO: generate version.h
+target("klotski-ffi")
+    set_kind("object")
+    add_includedirs(".")
+    add_files("ffi/*.cc")
+
+    set_configvar("COMPILER", "GCC ...")
+    set_configvar("BUILD_TIME", "...time...")
+
+    set_configdir("$(projectdir)/utils")
+    add_configfiles("utils/metadata.h.in", {
+        filename = "metadata.h", pattern = "@(.-)@"
+    })
+
+----------------------------------------------------------------
 
 target("klotski")
     set_kind("static")
-    add_includedirs(".")
-    add_files("ffi/*.cc")
+
+    add_deps("klotski-ffi")
     add_deps("utils", "all_cases")
     add_deps("raw_code", "short_code", "common_code")
     add_deps("core", "fast_cal", "analyse")
     add_deps("benchmark")
 
-    set_configdir("$(scriptdir)/utils")
-
-    set_configvar("COMPILER_INFO", "GNU ...")
-    set_configvar("BUILD_TIME", "...time...")
-
-    add_configfiles("utils/metadata.h.inx", {filename = "config.h"})
-
-    -- add_configfiles("config.h.in", {pattern = "@(.-)@"})
-
     set_policy("build.merge_archive", true)
---     set_targetdir("../../bin/")
+    set_targetdir("$(projectdir)/../../bin/")
 
---     os.cp("$(scriptdir)/*.h", "$(scriptdir)/../../bin/")
+    after_build(function (target)
+        os.cp("$(projectdir)/klotski.h", "$(projectdir)/../../bin/")
+    end)
 
 ----------------------------------------------------------------
