aed2b54dd1391b7b616bc867467b64ffd70bcb8c
Dnomd343 <i@343.re>
2023-02-28 20:07:59 +0800

    test: add rust basic ranges test

--- src/rust_ffi/src/all_cases.rs
+++ src/rust_ffi/src/all_cases.rs
@@ -11,42 +11,33 @@ lazy_static! {
     };
 }
 
-// struct BasicRange {
-//     data: Option<Vec<u32>>
-// }
-//
-// static mut BASIC_RANGE: BasicRange = BasicRange {
-//     data: None
-// };
-
-// impl BasicRange {
-    // fn fetch(&mut self) -> &Vec<u32> {
-        // match &self.data {
-        //     Some(data) => {
-        //         return data;
-        //     },
-        //     None => (),
-        // };
-
-        // &self.data.unwrap()
-    // }
-
-    // fn build(&mut self) {
-    //     unsafe {
-    //         Core::basic_ranges_build();
-    //         let mut buffer: Vec<u32> = vec![0; Core::BASIC_RANGES_SIZE as usize];
-    //         Core::export_basic_ranges(&mut buffer[0]);
-    //         self.data = Some(buffer);
-    //     }
-    // }
-// }
-
 pub fn demo() {
 
     println!("demo start");
 
-    println!("{:?}", (*BASIC_RANGES).len());
+
 
     println!("demo complete");
 
+}
+
+#[cfg(test)]
+mod tests {
+    use crypto::md5::Md5;
+    use crypto::digest::Digest;
+    use crate::all_cases::BASIC_RANGES;
+
+    const ALL_CASES_MD5: &str = "3888e9fab8d3cbb50908b12b147cfb23";
+    const BASIC_RANGES_MD5: &str = "6f385dc171e201089ff96bb010b47212";
+
+    #[test]
+    fn basic_ranges() {
+        let mut md5 = Md5::new();
+        let mut tmp = String::new();
+        for &range in &(*BASIC_RANGES) {
+            tmp.push_str(&format!("{:08X}\n", range));
+        }
+        md5.input_str(&tmp);
+        assert_eq!(md5.result_str(), BASIC_RANGES_MD5);
+    }
 }
