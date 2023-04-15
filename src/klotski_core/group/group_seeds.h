f977925fa438aa9331c6b3bf8c7b9de7a7008dfe
Dnomd343 <i@343.re>
2023-04-15 18:09:02 +0800

    chore: group seeds static content

--- src/klotski_core/group/group_seeds.h
+++ src/klotski_core/group/group_seeds.h
@@ -5,15 +5,15 @@
 namespace klotski {
 
 const uint64_t GROUP_SEEDS[25422] = {
-#include "static/group_seeds.inc"
+#include "group_seeds/content.inc"
 };
 
 const uint16_t GROUP_SEEDS_INDEX[25422] = {
-#include "static/group_seeds_index.inc"
+#include "group_seeds/index.inc"
 };
 
 const uint16_t GROUP_SEEDS_INDEX_REV[25422] = {
-#include "static/group_seeds_index_rev.inc"
+#include "group_seeds/index_rev.inc"
 };
 
 } // namespace klotski
