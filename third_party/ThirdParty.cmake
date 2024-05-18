115c431e20e7dd623bd6b13c98f1dae753072c31
Dnomd343 <dnomd343@gmail.com>
2024-05-18 15:11:54 +0800

    feat: python ffi support

--- third_party/ThirdParty.cmake
+++ third_party/ThirdParty.cmake
@@ -1,32 +1,37 @@
-set(KLOTSKI_THIRD_PARTY ${KLOTSKI_ROOT_DIR}/third_party)
+set(KLSK_THIRD_PARTY ${KLSK_ROOT_DIR}/third_party)
 
 # abseil library
 set(ABSL_PROPAGATE_CXX_STD ON)
-add_subdirectory(${KLOTSKI_THIRD_PARTY}/abseil-cpp EXCLUDE_FROM_ALL)
+add_subdirectory(${KLSK_THIRD_PARTY}/abseil-cpp EXCLUDE_FROM_ALL)
 
-if (KLOTSKI_ENABLE_BENCHMARK)
+if (KLSK_ENABLE_BENCHMARK)
   # google benchmark framework
   set(BENCHMARK_ENABLE_TESTING OFF)
   set(BENCHMARK_ENABLE_EXCEPTIONS OFF)
-  add_subdirectory(${KLOTSKI_THIRD_PARTY}/benchmark EXCLUDE_FROM_ALL)
+  add_subdirectory(${KLSK_THIRD_PARTY}/benchmark EXCLUDE_FROM_ALL)
 endif()
 
-if (KLOTSKI_ENABLE_TESTING)
+if (KLSK_ENABLE_TESTING)
   # BS thread pool
   add_library(thread_pool INTERFACE)
   target_include_directories(thread_pool INTERFACE
-          ${KLOTSKI_THIRD_PARTY}/thread-pool/include)
+          ${KLSK_THIRD_PARTY}/thread-pool/include)
   add_library(bs::thread_pool ALIAS thread_pool)
 
   # md5sum implementation
   set(MD5_ENABLE_LTO OFF)
-  add_subdirectory(${KLOTSKI_THIRD_PARTY}/md5sum EXCLUDE_FROM_ALL)
+  add_subdirectory(${KLSK_THIRD_PARTY}/md5sum EXCLUDE_FROM_ALL)
 
   # google test framework
-  add_subdirectory(${KLOTSKI_THIRD_PARTY}/googletest EXCLUDE_FROM_ALL)
+  add_subdirectory(${KLSK_THIRD_PARTY}/googletest EXCLUDE_FROM_ALL)
 
   # xxHash implementation
   add_library(xxhash INTERFACE)
-  target_include_directories(xxhash INTERFACE ${KLOTSKI_THIRD_PARTY}/xxHash)
+  target_include_directories(xxhash INTERFACE ${KLSK_THIRD_PARTY}/xxHash)
   add_library(xxHash::xxh3 ALIAS xxhash)
+endif()
+
+if (KLSK_PYTHON_FFI)
+  # python ffi exposure
+  add_subdirectory(${KLSK_THIRD_PARTY}/pybind11 EXCLUDE_FROM_ALL)
 endif()
