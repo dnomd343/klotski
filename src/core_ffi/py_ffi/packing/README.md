9d09113280923fa82d107bd81436be65b6ebed80
Dnomd343 <dnomd343@gmail.com>
2024-11-16 17:57:50 +0800

    build: python packing configuration

--- src/core_ffi/py_ffi/packing/README.md
+++ src/core_ffi/py_ffi/packing/README.md
@@ -1,1 +1,7 @@
-# klotski python ffi
+# Klotski Engine
+
+> Klotski multifunctional engine with high performance.
+
+The project is under development and is currently in feature preview.
+
+Note that the interfaces are not stable yet, and documentation will be added in subsequent versions.
--- src/core_ffi/py_ffi/packing/setup.py
+++ src/core_ffi/py_ffi/packing/setup.py
@@ -1,14 +1,22 @@
 from skbuild import setup
 
 setup(
-    name="py-klotski",
-    version="0.0.1",
-    description="klotski engine",
-    author="Dnomd343",
-    license="MIT",
-    packages=["klotski"],
-    package_dir={"": "src"},
-    cmake_install_dir="src/klotski",
-    cmake_args=['-DKLSK_ENABLE_TESTING=OFF', '-DKLSK_ENABLE_BENCHMARK=OFF', '-DKLSK_C_FFI=OFF', '-DKLSK_RUST_FFI=OFF', '-DKLSK_PYTHON_FFI=ON'],
-    python_requires=">=3.9",
+    name='py-klotski',
+    version='0.0.1',
+    description='Klotski multifunctional engine with high performance',
+    author='Dnomd343',
+    author_email='dnomd343@gmail.com',
+    license='BSD-3-Clause',
+    url='https://github.com/dnomd343/klotski',
+    packages=['klotski'],
+    package_dir={'': 'src'},
+    cmake_install_dir='src/klotski',
+    cmake_args=[
+        '-DKLSK_ENABLE_TESTING:BOOL=OFF',
+        '-DKLSK_ENABLE_BENCHMARK:BOOL=OFF',
+        '-DKLSK_C_FFI:BOOL=OFF',
+        '-DKLSK_RUST_FFI:BOOL=OFF',
+        '-DKLSK_PYTHON_FFI:BOOL=ON',
+    ],
+    python_requires=">=3.7",
 )
