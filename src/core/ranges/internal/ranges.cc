4121ca229e3492df9e9722262a3ee479114529d5
Dnomd343 <dnomd343@gmail.com>
2024-11-17 14:07:33 +0800

    update: enhance ranges module

--- src/core/ranges/internal/ranges.cc
+++ src/core/ranges/internal/ranges.cc
@@ -1,3 +1,5 @@
+#include <span>
+
 #include "ranges/ranges.h"
 #include "common_code/common_code.h"
 
@@ -5,18 +7,10 @@ using klotski::cases::Ranges;
 using klotski::codec::CommonCode;
 using klotski::cases::RangesUnion;
 
-RangesUnion& RangesUnion::operator+=(const RangesUnion &ranges_union) {
-    for (const auto head : Heads) {
-        ranges(head) += ranges_union.ranges(head);
-    }
-    return *this;
-}
-
 std::vector<CommonCode> RangesUnion::codes() const {
     std::vector<CommonCode> codes;
     codes.reserve(size());
     for (const auto head : Heads) {
-        // for (const auto range : (*this)[head]) {
         for (const auto range : ranges(head)) {
             codes.emplace_back(CommonCode::unsafe_create(head << 32 | range));
         }
@@ -26,26 +20,19 @@ std::vector<CommonCode> RangesUnion::codes() const {
 }
 
 KLSK_INLINE CommonCode RangesUnion::operator[](size_type n) const {
-    // uint64_t head = 0;
-    // while (n >= ranges(head).size()) {
-    //     n -= ranges(head).size();
-    //     ++head;
-    // }
-    // KLSK_ASSUME(head < 16);
-    // return CommonCode::unsafe_create(head << 32 | ranges(head)[n]);
-
     if (n < ranges(0).size()) {
         return CommonCode::unsafe_create(ranges(0)[n]);
     }
     n -= ranges(0).size();
 
-    KLSK_UNROLL(sizeof(Heads) - 2)
-    for (const uint64_t head : std::to_array({0x1, 0x2, 0x4, 0x5, 0x6, 0x8, 0x9, 0xA, 0xC, 0xD})) {
+    KLSK_UNROLL(Heads.size() - 2)
+    for (const uint64_t head : std::span {Heads.data() + 1, Heads.size() - 2}) {
         if (n < ranges(head).size()) {
             return CommonCode::unsafe_create(head << 32 | ranges(head)[n]);
         }
         n -= ranges(head).size();
     }
 
-    return CommonCode::unsafe_create((uint64_t)0xE << 32 | ranges(0xE)[n]);
+    constexpr auto head = static_cast<uint64_t>(0xE);
+    return CommonCode::unsafe_create(head << 32 | ranges(0xE)[n]);
 }
