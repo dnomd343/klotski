8070ae389d5130ef06a33cae9f2b645fe4bd15ff
Dnomd343 <dnomd343@gmail.com>
2024-11-24 15:17:30 +0800

    build: optimize cmake structure

--- src/core_ffi/rust_ffi/build.rs
+++ src/core_ffi/rust_ffi/build.rs
@@ -1,27 +1,23 @@
-extern crate cmake;
+// NOTE: add the following environment variables for using llvm toolchain
+//         CC=clang-20
+//         CXX=clang++-20
+//         CXXSTDLIB="c++"
+//         CXXFLAGS="-stdlib=libc++"
+//         RUSTFLAGS="-Clinker=clang-20 -Clink-arg=-fuse-ld=lld-20 -Clink-arg=-stdlib=libc++"
 
 use cxx_build::CFG;
 
-// NOTE: add `CC=clang-20 CXX=clang++-20 CXXFLAGS="-stdlib=libc++"` for cargo command
-//       we should keep cxx crate using clang for `cxx.cc`
-
-// NOTE: also, `RUSTFLAGS="-C linker=clang-20"` should be added to cargo env for using lld
-
-// NOTE: add `CC=clang-20 CXX=clang++-20 CXXFLAGS="-stdlib=libc++" RUSTFLAGS="-C linker=clang-20 -C link-arg=-fuse-ld=lld-20 -C link-arg=-stdlib=libc++"` for using llvm toolchain
-
-// NOTE: it seems that cxx_build link `libstdc++` on linux, we can add `-C link-arg=-lc++ -C link-arg=-lc++abi` for workaround, but it should be fixed in the future.
-
 fn main() {
-    let dst = cmake::Config::new("klotski")
+    cmake::Config::new("klotski")
         .define("CARGO_BUILD:BOOL", "ON")
         .define("KLSK_ENABLE_TESTING:BOOL", "OFF")
         .define("KLSK_ENABLE_BENCHMARK:BOOL", "OFF")
         .define("KLSK_C_FFI:BOOL", "OFF")
         .define("KLSK_PYTHON_FFI:BOOL", "OFF")
         .build();
+    println!("cargo:rustc-link-lib=static=klotski_core");
 
     CFG.include_prefix = "rust_ffi";
-
     cxx_build::bridge("src/bridge.rs")
         .file("adapter/short_code.cc")
         .file("adapter/common_code.cc")
@@ -30,9 +26,6 @@ fn main() {
         .flag("-fno-rtti")
         .flag("-fno-exceptions")
         .compile("klotski");
-
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=klotski_core");
 
     println!("cargo:rerun-if-changed=src/bridge.rs");
     println!("cargo:rerun-if-changed=adapter/short_code.cc");
