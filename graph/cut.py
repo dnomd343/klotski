513859b668c9f1657824563bbe38afe4789c942c
Dnomd343 <dnomd343@gmail.com>
2024-12-07 17:46:18 +0800

    feat: add support of ring folding

--- graph/cut.py
+++ graph/cut.py
@@ -3,7 +3,7 @@
 import igraph as ig
 
 
-def split_max_graph(g: ig.Graph, cut_point: int):
+def split_max_graph(g: ig.Graph, cut_point: int) -> ig.Graph:
     gg = g.copy()
     edges = [(cut_point, x.index) for x in gg.vs[cut_point].neighbors()]
     gg.delete_edges(edges)
@@ -11,33 +11,17 @@ def split_max_graph(g: ig.Graph, cut_point: int):
     return g.subgraph(components[0] + [cut_point])
 
 
-def split_subgraph(g: ig.Graph):
-
-    # def demo():
-    #     sizes = set()
-    #     for cut_point in g.articulation_points():
-    #         gg: ig.Graph = g.copy()
-    #         gg.delete_vertices(cut_point)
-    #         subs = gg.decompose()
-    #         sizes.update([x.vcount() for x in subs])
-    #     print(sorted(sizes))
-
-    # demo()
-
-    for _ in range(1034):
-        print(g.vcount(), g.ecount())
-
+def split_subgraph(g: ig.Graph) -> ig.Graph:
+    while True:
         points = g.articulation_points()
-        assert len(points) > 0
+        if len(points) == 0:
+            break
         g = split_max_graph(g, points[0])
-
-    # ig.plot(g, 'demo.png', bbox=(2000, 2000))
-    print(g.vcount(), g.ecount())
-    # g.write_graphml('main_cir.graphml')
-    g.write_pickle('main_cir.pkl')
+        print(g.vcount(), g.ecount())
+    return g
 
 
 if __name__ == '__main__':
     raw = ig.Graph.Read_Pickle('main_combined.pkl')
-    # print(raw.vcount())
-    split_subgraph(raw)
+    split_subgraph(raw).write_pickle('main_cut.pkl')
+    # split_subgraph(raw).write_graphml('main_cut.graphml')
