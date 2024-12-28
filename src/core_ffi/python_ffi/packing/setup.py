7dea98a702a66a56427f9ecef8c6d808cbf02c02
Dnomd343 <dnomd343@gmail.com>
2024-12-28 12:15:01 +0800

    build: update python package configure

--- src/core_ffi/python_ffi/packing/setup.py
+++ src/core_ffi/python_ffi/packing/setup.py
@@ -1,22 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from skbuild import setup
 
 setup(
-    name='py-klotski',
-    version='0.0.1',
-    description='Klotski multifunctional engine with high performance',
-    author='Dnomd343',
-    author_email='dnomd343@gmail.com',
-    license='BSD-3-Clause',
-    url='https://github.com/dnomd343/klotski',
     packages=['klotski'],
     package_dir={'': 'src'},
     cmake_install_dir='src/klotski',
     cmake_args=[
-        '-DKLSK_ENABLE_TESTING:BOOL=OFF',
-        '-DKLSK_ENABLE_BENCHMARK:BOOL=OFF',
-        '-DKLSK_C_FFI:BOOL=OFF',
-        '-DKLSK_RUST_FFI:BOOL=OFF',
+        '-DKLSK_ENABLE_LTO:BOOL=ON',
         '-DKLSK_PYTHON_FFI:BOOL=ON',
-    ],
-    python_requires=">=3.7",
+    ]
 )
